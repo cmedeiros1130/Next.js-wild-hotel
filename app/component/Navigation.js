f0a45bae8ae0ac9c6d1a48408a549b867963113d
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-09-09 04:13:40 -0400

    Updated the project with new changes

---
+++ app/component/Navigation.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/cabins">cabins</Link>
+      </li>
+      <li>
+        <Link href="/about">about</Link>
+      </li>
+      <li>
+        <Link href="/account">your account</Link>
+      </li>
+    </ul>
+  );
+}
